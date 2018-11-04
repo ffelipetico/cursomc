65daf7cc04c23bbacc2c3a630a7502b7ef4c2883
Felipe Afonso <ffelipesiqueira@gmail.com>
2018-11-04 12:57:02 -0200

    Estado e Cidade

--- src/main/java/com/biricutico/cursomc/CursomcApplication.java
+++ src/main/java/com/biricutico/cursomc/CursomcApplication.java
@@ -8,18 +8,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.biricutico.cursomc.domain.Categoria;
+import com.biricutico.cursomc.domain.Cidade;
+import com.biricutico.cursomc.domain.Estado;
 import com.biricutico.cursomc.domain.Produto;
 import com.biricutico.cursomc.repositories.CategoriaRepository;
+import com.biricutico.cursomc.repositories.CidadeRepository;
+import com.biricutico.cursomc.repositories.EstadoRepository;
 import com.biricutico.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
-	private CategoriaRepository categoriaRepository;
-	
+	private CategoriaRepository categoriaRepository;	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -47,6 +54,25 @@ public class CursomcApplication implements CommandLineRunner{
 		//salva as categorias e os produtos
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		////
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		//no próprio construtor já fazemos a associação de muitos pra um
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		//primeiro salva estado, pois o estado tem várias cidades
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 		
 	}
 }
