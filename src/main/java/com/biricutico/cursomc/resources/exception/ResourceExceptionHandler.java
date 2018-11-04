becd294f8827082bae1dd373e1cd950edc4ab1ed
Felipe Afonso <ffelipesiqueira@gmail.com>
2018-11-03 23:27:00 -0300

    Ajuste no endpoint /categoria/{id}

---
+++ src/main/java/com/biricutico/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.biricutico.cursomc.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.biricutico.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	//esse método vai recer a exceção e as informações da requisição
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
